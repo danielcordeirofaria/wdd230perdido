b61e6d199c7cb796f773afdca19215fb9b0f81b7
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-09-16 17:20:25 -0300

    modificações feitas

---
+++ scripts/getdates.js
@@ -0,0 +1,12 @@
+const lastModifiedHTML = document.querySelector(".lastModified")
+
+const lastModifiedDate = new Date(document.lastModified)
+const day = lastModifiedDate.getDate()
+const month = lastModifiedDate.getMonth() + 1
+const year = lastModifiedDate.getFullYear()
+const hour = String(lastModifiedDate.getHours()).padStart(2, '0')
+const minutes = String(lastModifiedDate.getMinutes()).padStart(2, '0')
+const seconds = String(lastModifiedDate.getSeconds()).padStart(2, '0')
+
+
+lastModifiedHTML.innerText = `Last Modification: ${month}/${day}/${year} ${hour}:${minutes}:${seconds}`
