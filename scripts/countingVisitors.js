7e11e6a78479f718fb63410458e830d4d30051fc
danielcordeirofaria <danielcordeirofaria@gmail.com>
2023-11-18 19:29:35 -0300

    adding the weather on the  course home page

--- scripts/countingVisitors.js
+++ scripts/countingVisitors.js
@@ -1,5 +1,5 @@
 // 1️⃣ Initialize display element variable
-const visitsDisplay = document.querySelector(".numberOfBisitation");
+const visitsDisplay = document.querySelector(".numberOfVisitation");
 
 // 2️⃣ Get the stored VALUE for the numVisits-ls KEY in localStorage if it exists. If the numVisits KEY is missing, then assign 0 to the numVisits variable.
 let numVisits = Number(window.localStorage.getItem("numVisits-ls")) || 0;
---
+++ scripts/weather.js
@@ -0,0 +1,52 @@
+const cardPageInformation = document.querySelector('.pageInformation')
+
+const lat = -25.49;
+const long = -49.34;
+const key = "3fd567ec38f593abe6ddc29dd6159dcd";
+
+const url = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&units=imperial&appid=${key}`;
+
+async function apiFetch() {
+    try {
+        let response = await fetch(url);
+        if (response.ok) {
+            const data = await response.json();
+            console.log(data)
+            displayResults(data);
+        } else {
+            throw new Error(await response.text());
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function displayResults(data) {
+    const creatingDivWeather = document.createElement('div')
+    creatingDivWeather.classList.add('divWeather')
+    cardPageInformation.appendChild(creatingDivWeather)
+
+    const pWeatherInformation = document.createElement('h4')
+    pWeatherInformation.classList.add('textInformationWeather')
+
+    const iconsrc = `https://openweathermap.org/img/wn/${data.weather[0].icon}.png`;
+
+    const weatherIcon = document.createElement('img');
+    weatherIcon.setAttribute('src', iconsrc);
+    weatherIcon.setAttribute('alt', 'Weather Icon');
+    weatherIcon.setAttribute('width', 50);
+
+    pWeatherInformation.appendChild(weatherIcon);
+    pWeatherInformation.innerHTML += ` ${data.main.temp.toFixed(1)}°F - ${capitalizeFirstLetter(data.weather[0].description)}`;
+
+    creatingDivWeather.appendChild(pWeatherInformation);
+    
+
+
+}
+
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+apiFetch();
